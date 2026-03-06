0613efab5b99e5057a2a594f2025bb4f813576e7
Gustavo Gobbato <52713981+gugagobbato@users.noreply.github.com>
2026-03-06 14:45:26 -0300

    Update Program.cs

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var tasks = new List<TaskItem>();
+
+app.MapGet("/", () => "Minimal API funcionando!");
+
+app.MapGet("/tasks", () => tasks);
+
+app.MapPost("/tasks", (TaskItem task) =>
+{
+    task.Id = tasks.Count + 1;
+    tasks.Add(task);
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
+app.MapGet("/tasks/{id}", (int id) =>
+{
+    var task = tasks.FirstOrDefault(t => t.Id == id);
+
+    if (task is null)
+        return Results.NotFound();
+
+    return Results.Ok(task);
+});
 
 app.Run();
+
+record TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public bool IsCompleted { get; set; }
+}
